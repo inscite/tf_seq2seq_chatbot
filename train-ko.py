77437bde7d479fb0162887ee3afc074baa68519c
Seungkyun Hong (G133) <xo@kisti.re.kr>
2016-12-28 20:13:22 +0900

    Hotfix
    - fixed TensorFlow requirements (above 0.9)
    - simple txt edit

--- train-ko.py
+++ train-ko.py
@@ -4,9 +4,7 @@ import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 import tensorflow as tf
-
 from tf_seq2seq_chatbot.lib.ko.train import train
-
 
 def main(_):
     train()
